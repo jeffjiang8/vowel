065d0cd17577173b1351fd5b9e18287a68499ae2
Jeff Jiang <jeffjiang8@gmail.com>
2020-05-13 19:25:30 -0400

    2.0

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function vowels(str) {
 
+    // let count = 0
+    // const letters = 'aeiou' 
+    // // or const letters = ['a', 'e', 'i', 'o', 'u']
+    // for (let char of str.toLowerCase()) {
+    //     if (letters.includes(char)) {
+    //         count ++
+    //     }
+    // }
+    // return count
 
     const matches = str.match(/[aeiou]/gi)
     // g => make sure the function doesn't stop after finding the first one, i => insensitive
